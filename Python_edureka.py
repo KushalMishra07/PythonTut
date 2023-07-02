155f795af9559310782ee9963edbb1a629952220
KushalMishra07 <godsownenvoy@gmail.com>
2023-07-02 17:17:10 +0530

    Adding Pythonedureka version

---
+++ Python_edureka.py
@@ -0,0 +1,45 @@
+# print("hello from Python Edureka chapter")
+# ##Comparison Operator
+# print(2>3) ##result will be boolean that is false
+# print(2<=3)##resut will be a boolean that is true
+# ##Logical Operators
+# age = 18
+# print(age>1 and age<20)##this will use logical operator AND and return true
+# print(age>89 or age<2) ##False)
+# print(not age>2) ##using the not operator
+# ##and both true
+# ##or atleast 1 true
+# ##not
+# ##Augmented Assignment
+# x = 10
+# # x = x+30
+# print(x+=10)
+# ##Better way
+# y = 10
+# y+=30
+# print(y)
+# if (y>=100):
+#     print("printing from 1st loop")
+#     print("Leaving the team like that")
+# elif (y>=3 and y<=50):
+#     print("Second loop testing")
+# i=0
+# j =1
+# while i<=100:
+#     while j<=1000:
+#         print(j)
+#         j+=100
+#     print(i)
+#     i+=1
+i=0
+j=0
+
+while i<10:
+    while j<20:
+        print('#'*j)
+        j+=1
+    j=0 #Resetting the J counter for correct printing
+    i+=1
+
+
+help(print)
