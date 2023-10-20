48e374eda5bf1cac595a3d95f1930f0d264dd51d
KushalMishra07 <godsownenvoy@gmail.com>
2023-10-20 23:20:06 +0530

    DSA Challenge Day 4

---
+++ Arrays/Mean.py
@@ -0,0 +1,10 @@
+
+def mean(nums):
+    if not nums:
+        raise ValueError("List is empty")
+    return sum(nums) / len(nums)
+
+
+if __name__ == "__main__":
+    nums = [1,2,3,4,5]
+    print(mean(nums))
